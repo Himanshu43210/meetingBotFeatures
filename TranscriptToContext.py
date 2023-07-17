8d8f904cb3a0803a8f16a4d294579ca0178be6c4
IshaTechhelps <138512824+ishaTechhelps@users.noreply.github.com>
2023-07-17 18:15:17 +0530

    Update TranscriptToContext.py

--- TranscriptToContext.py
+++ TranscriptToContext.py
@@ -1,9 +1,6 @@
 import openai
 import boto3
 
-# Set up your OpenAI API credentials
-openai.api_key = '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'
-
 def generate_conversation(meeting_notes):
     dynamicPrompt = '''
 please provide me the meeting notes for this diary text in the form of parsed JSON, with the title as meeting note and content as the resulting meeting note: '''
@@ -30,36 +27,44 @@ def write_text_file_to_s3(bucket, key, content, aws_access_key, aws_secret_key):
     s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
     s3.put_object(Bucket=bucket, Key=key, Body=content)
 
-def list_files_in_bucket(bucket, aws_access_key, aws_secret_key):
+def search_files_in_bucket(bucket, search_term, aws_access_key, aws_secret_key):
     s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
     response = s3.list_objects_v2(Bucket=bucket)
-    files = []
+    list_of_files = []
 
     # Iterate over the objects in the bucket
     for obj in response['Contents']:
-        files.append(obj['Key'])
+        file_name=(obj['Key'])
 
-    return files
+        # Check if the file contains the search term
+        if search_term in file_name:
+            list_of_files.append(file_name)
+
+
+    return list_of_files
+
+def contextFunctionality(inputFile=""):
+    outputFile = inputFile.replace("Diarization_","TranscriptToContext_")
+    # Read the input text file from S3
+    meeting_notes = read_text_file_from_s3(bucket, inputFile, aws_access_key, aws_secret_key)
+    # Generate conversation context
+    conversation_context = generate_conversation(meeting_notes)
+    # Write the output text file to S3
+    write_text_file_to_s3(bucket, outputFile, conversation_context, aws_access_key, aws_secret_key)
 
 # Example usage
 bucket = 'meeting-bot-processed-files'
-input_file_key = 'transcript.txt'
-output_file_key = 'output1.txt'
+openai.api_key = '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'
 aws_access_key = 'AKIAVSYBEZQGCM6UUNMM'
 aws_secret_key = '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'
+search_term= 'Diarization_'
 
 # List files in the S3 bucket
-file_list = list_files_in_bucket(bucket, aws_access_key, aws_secret_key)
+list_of_files = search_files_in_bucket(bucket, search_term, aws_access_key, aws_secret_key)
 
 # Print the file names
-for file_name in file_list:
+for file_name in list_of_files:
     print(file_name)
+    contextFunctionality(file_name)
+    print(file_name+ " Processed Successfully")
 
-# Read the input text file from S3
-meeting_notes = read_text_file_from_s3(bucket, input_file_key, aws_access_key, aws_secret_key)
-
-# Generate conversation context
-conversation_context = generate_conversation(meeting_notes)
-
-# Write the output text file to S3
-write_text_file_to_s3(bucket, output_file_key, conversation_context, aws_access_key, aws_secret_key)
